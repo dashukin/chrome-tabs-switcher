0b4d17102e9b2ad3dba5140733ba4fd4dadc20c4
dashukin <dashukin@gmail.com>
2017-02-05 20:35:52 +0300

    Update contentscript to allow app to be injected into the page

--- src/app/js/contentscript.js
+++ src/app/js/contentscript.js
@@ -1,1 +1,25 @@
 // Content script
+
+import appConfig from './app.config';
+import React from 'react';
+import ReactDom from 'react-dom';
+import Application from './components/application';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+let appId = String.prototype.toLowerCase.call(appConfig.appId || '').replace(/\s/g, '');
+
+let appHolder = document.createElement('div');
+appHolder.id = appId;
+appHolder.className = appId;
+
+document.body.appendChild(appHolder);
+
+const App = () => {
+	return (
+		<MuiThemeProvider>
+			<Application />
+		</MuiThemeProvider>
+	);
+}
+
+ReactDom.render(<App/>, appHolder);
